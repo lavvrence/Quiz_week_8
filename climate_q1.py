ca2e042d6c64a7c93d98a42f9e3854a1c0e35b25
Lawrence.C <lawrencechen3m@hotmail.com>
2023-09-14 11:36:46 +1000

    connected database, added query, sorting query data

--- climate_q1.py
+++ climate_q1.py
@@ -1,8 +1,26 @@
+import sqlite3
 import matplotlib.pyplot as plt
-        
+
+#connect to database
+con = sqlite3.connect('climate.db')
+cursor = con.cursor()
+
+
+# query for retrieving data
+cursor.execute("SELECT * FROM ClimateData")
+
 years = []
 co2 = []
 temp = []
+
+# sorting data from query
+for row in cursor.fetchall():
+    years.append(row[0])
+    co2.append(row[1])
+    temp.append(row[2])
+
+# close connection
+con.close()
 
 plt.subplot(2, 1, 1)
 plt.plot(years, co2, 'b--') 
